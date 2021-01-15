277d7f46327d6448db827654a61de93c43556086
Makoto Motohashi <mkt.motohashi@gmail.com>
2021-01-15 14:28:58 +0900

    Remove dollar sign in favor of getElementsByClassName

--- webview.js
+++ webview.js
@@ -1,8 +1,8 @@
 module.exports = (Franz) => {
   const getUnreads = () => {
-    const e = $('.badge--notification');
-		if (e) {
-			Franz.setBadge(Number(e.text()));
+    const e = document.getElementsByClassName('badge--notification');
+		if (e && e.length > 0) {
+			Franz.setBadge(Number(e[0].text()));
 		} else {
 			Franz.setBadge(0)
 		}
